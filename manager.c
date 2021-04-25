c41d57c1ab859807d3c8f949f623cd18935389bb
didgmlcks99 <heechy06@naver.com>
2021-04-25 18:49:57 +0900

    load

--- manager.c
+++ manager.c
@@ -55,12 +55,22 @@ void saveData(Product p[], int count){
 int loadData(Product *p){
 	int count=0;
 	FILE*fp;
+	
+	fp = fopen("product.txt", "rt");
+	if(fp == NULL){
+		printf("파일 없음!\n");
+		return 0;
+	}
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
+	for(int i=0; i<100; i++){
+		fscanf(fp, "%d %d %[^\n]s", &p[i].weight, &p[i].price, p[i].name);
+		if(feof(fp)) break;
+		count++;
+	}
 
-
-
-
+	fclose(fp);
+	
 	printf("=> 로딩 성공!\n");
 	return count;
 }
